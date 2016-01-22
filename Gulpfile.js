f98e2a56ba407281aa8df3f0fa22fa1dd9e421b8
FernandoGomes <fernandogomes90@gmail.com>
2016-01-22 17:49:17 +0000

    first

---
+++ Gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+    sass = require('gulp-sass');
+    autoprefixer = require('gulp-autoprefixer');
+    cssnano = require('gulp-cssnano');
+    livereload = require('gulp-livereload');
+    rename = require('gulp-rename');
+
+gulp.task('sass', function(){
+  return gulp.src('sass/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({browsers:['last 2 versions'],cascade: false}))
+      .pipe(gulp.dest('./css/'))
+      .pipe(rename({suffix:'.min'}))
+      .pipe(cssnano())
+      .pipe(gulp.dest('./css/'));
+});
+gulp.task('watch',function() {
+    gulp.watch('sass/*.scss',['sass']);
+    livereload.listen();
+    gulp.watch(['css/**']).on('change',livereload.changed);
+});
+gulp.task('default', ['sass']);
